6eda05d518e2052271fe7aca88e4d9f70367b2a5
Paloma Cardoso <paloma.cardoso10@gmail.com>
2022-12-24 16:34:50 -0300

    feat(routes): cria rotas e layout de rotas com React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
+import { Router } from './Routes'
 
 export function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
-      Olá mundo
     </ThemeProvider>
   )
 }
