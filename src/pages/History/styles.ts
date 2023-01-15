2e9cb09683a53198b17212a61588a83f8e3bc888
Paloma Cardoso <paloma.cardoso10@gmail.com>
2023-01-15 17:25:44 -0300

    feat(countdown): acrescenta contexto a home para passar os dados do form e countdown

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -8,7 +8,7 @@ export const HistoryContainer = styled.main`
 
   h1 {
     font-size: 1.5rem;
-    color: ${props => props.theme['gray-100']};
+    color: ${(props) => props.theme['gray-100']};
   }
 `
 export const HistoryList = styled.div`
@@ -22,10 +22,10 @@ export const HistoryList = styled.div`
     min-width: 600px;
 
     th {
-      background-color: ${props => props.theme['gray-600']};
+      background-color: ${(props) => props.theme['gray-600']};
       padding: 1rem;
       text-align: left;
-      color: ${props => props.theme['gray-100']};
+      color: ${(props) => props.theme['gray-100']};
       font-size: 0.875rem;
       line-height: 1.6rem;
 
@@ -41,8 +41,8 @@ export const HistoryList = styled.div`
     }
 
     td {
-      background-color: ${props => props.theme['gray-700']};
-      border-top: 4px solid ${props => props.theme['gray-800']};
+      background-color: ${(props) => props.theme['gray-700']};
+      border-top: 4px solid ${(props) => props.theme['gray-800']};
       padding: 1rem;
       font-size: 0.875rem;
       line-height: 1.6rem;
@@ -61,7 +61,7 @@ export const HistoryList = styled.div`
 const STATUS_COLORS = {
   yellow: 'yellow-500',
   green: 'green-500',
-  red: 'red-500'
+  red: 'red-500',
 } as const
 
 interface StatusProps {
@@ -78,6 +78,6 @@ export const Status = styled.span<StatusProps>`
     width: 0.5rem;
     height: 0.5rem;
     border-radius: 99999px;
-    background: ${props => props.theme[STATUS_COLORS[props.statusColor]]};
+    background: ${(props) => props.theme[STATUS_COLORS[props.statusColor]]};
   }
 `
---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,43 @@
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+
+import { useContext } from 'react'
+import { CyclesContext } from '../..'
+import { useFormContext } from 'react-hook-form'
+
+export function NewCycleForm() {
+  const { activeCycle } = useContext(CyclesContext)
+  const { register } = useFormContext()
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        type="text"
+        id="task"
+        placeholder="Dê um nome para seu projeto"
+        list="task-suggestions"
+        {...register('task')}
+        disabled={!!activeCycle}
+      />
+      <datalist id="task-suggestions">
+        <option value="projeto 1" />
+        <option value="projeto 2" />
+        <option value="projeto 3" />
+        <option value="banana" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={1}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
